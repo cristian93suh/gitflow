8e785fb261d8308fd963d911c211550e853f465d
cristian93suh <alloggio.cristian93@gmail.com>
2024-06-19 11:25:40 +0200

    push progetto esempio gitflow

---
+++ src/main/java/com/example/gitflowExample/GitflowExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitflowExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitflowExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitflowExample/GitflowExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitflowExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
